24f3f02885d1f0b87d2defe2f53f0bbfec49ed21
Willians Faria <dev.willians.faria@gmail.com>
2024-02-14 17:20:26 -0300

    finished implementation of hashtable

--- src/yem_ht.c
+++ src/yem_ht.c
@@ -44,19 +44,26 @@ void yem_ht_push(struct yem_ht* ht, char* key, int value) {
     item->value = value;
     item->next = malloc(sizeof(struct yem_ht_item));
 
-    struct yem_ht_item existing = ht->items[idx];
+    struct yem_ht_item* curr = &ht->items[idx];
+    struct yem_ht_item* prev = &ht->items[idx];
 
-    if (existing.key != NULL) {
-        struct yem_ht_item* curr = &ht->items[idx];
-        while (curr->next->key != NULL) {
-            curr = curr->next;
-        }
-        curr->next = item;
+    if (curr->key == NULL) {
+        ht->items[idx] = *item;
+        ++ht->length;
         return;
     }
 
-    ht->items[idx] = *item;
-    ++ht->length;
+    while (curr->key != NULL) {
+        if (curr->key == key) {
+            item->next = curr->next;
+            ht->items[idx] = *item;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    prev->next = item;
 }
 
 struct yem_ht_item* yem_ht_get(struct yem_ht* ht, char* key) {
@@ -71,4 +78,25 @@ struct yem_ht_item* yem_ht_get(struct yem_ht* ht, char* key) {
     }
 
     return curr;
+}
+
+void yem_ht_del(struct yem_ht* ht, char* key) {
+    uint64_t idx = hash_key(key) % ht->capacity;
+
+    struct yem_ht_item* curr = &ht->items[idx];
+    struct yem_ht_item* prev = &ht->items[idx];
+
+    if (curr->key == key) {
+        ht->items[idx] = *curr->next;
+        return;
+    }
+
+    while (curr->key != NULL) {
+        if (curr->key == key) {
+            prev->next = curr->next;
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
 }
